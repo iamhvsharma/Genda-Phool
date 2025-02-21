30a7aec4bf3be2404172a11ae1e4ca71b1863d74
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-02-21 22:28:58 +0530

    health

--- server.js
+++ server.js
@@ -26,5 +26,11 @@ mongoose.connect(process.env.MONGODB_URI, {
 // Routes
 app.use('/api/auth', authRoutes);
 
+app.get('health', (req, res) => {
+  res.json({
+    MSG: "BACKEND IS RUNNING !"
+  })
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
